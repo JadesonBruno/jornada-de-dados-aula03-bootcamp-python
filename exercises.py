ea59c724cad5c213a659da0f59c25acf3ce0170b
JadesonBruno <jadesonbruno.a@outlook.com>
2025-03-01 21:17:29 -0300

    :sparkles: feat: Adding resolution of 2nd  exercise

--- exercises.py
+++ exercises.py
@@ -3,7 +3,7 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
-try:
+""" try:
     quantity = int(input("Enter quantity: "))
     price = float(input("Enter price: "))
 
@@ -13,12 +13,26 @@ try:
         print("Error: Invalid data")
 
 except ValueError as e:
-    print(f"Error, invalid input: {e}")
+    print(f"Error, invalid input: {e}") """
 
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
+try:
+    temperature = float(input("Enter a temperature: "))
+
+    if temperature < 18:
+        print("Temperature is low.")
+    elif 18 <= temperature <= 26:
+        print("Temperature is normal.")
+    else:
+        print("Temperature is high.")
+except ValueError as e:
+    print(f"Error, invalid input: {e}")
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
